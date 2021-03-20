a6ed1169c6134aa627a0fbe09aad3e82732d05ee
Roman Kovalov <POMKOV2000@gmail.com>
2021-03-21 00:13:48 +0200

    +registerOnServer()->connectRegTcpSocket(); +registerOnServer()->sendClientRecipientData()

--- MultiplayerProject_Client/NetworkClient.cpp
+++ MultiplayerProject_Client/NetworkClient.cpp
@@ -12,17 +12,61 @@ Socket::Status NetworkClient::init(unsigned short preferablePort)
 		return Socket::Status::Done;
 	else
 	{
-		cout << "init(): Failed to bind passed preferred port\n";
+		cout << "(!)init(): Failed to bind passed preferred port\n";
 		do
 		{
 			unsigned short newPort = Socket::AnyPort;
 			cout << "init(): Trying to bind other port - " << newPort << endl;
 			status = dataSocket.bind(newPort);
 			if (status != Socket::Status::Done)
-				cout << "init(): Failed to bind other port. Retrying...\n";
+				cout << "(!)init(): Failed to bind other port. Retrying...\n";
 
 		} while (status != Socket::Done);
 
 		cout << "init(): Successfully binded to other port - " << dataSocket.getLocalPort() << endl;
+		return Socket::Status::Done;
+	}
+}
+
+Socket::Status NetworkClient::registerOnServer(IpAddress serverIp, unsigned short serverRegPort, string clientName)
+{
+	if (connectRegTcpSocket(serverIp, serverRegPort) != Socket::Status::Done)
+		return Socket::Status::Error;
+
+	if (sendClientRecipientData(clientName) != Socket::Status::Done)
+		return Socket::Status::Error;
+	
+}
+
+Socket::Status NetworkClient::connectRegTcpSocket(IpAddress serverIp, unsigned short serverRegPort)
+{
+	if (regSocket.connect(serverIp, serverRegPort) == Socket::Status::Done)
+	{
+		cout << "registerOnServer()->connectRegTcpSocket(): Connected to server\n";
+		return Socket::Status::Done;
+	}
+	else
+	{
+		cout << "(!)registerOnServer()->connectRegTcpSocket(): Error connecting to server!\n";
+		return Socket::Status::Error;
+	}
+}
+
+Socket::Status NetworkClient::sendClientRecipientData(string clientName)
+{
+	if (!regSocket.isBlocking()) regSocket.setBlocking(true);
+
+	Packet tempPacket;
+	tempPacket << dataSocket.getLocalPort() << clientName;
+
+	if (regSocket.send(tempPacket) == Socket::Status::Done)
+	{
+		cout << "registerOnServer()->sendClientRecipientData(): Successfully sent client recipient data\n";
+		return Socket::Status::Done;
+	}
+	else
+	{
+		cout << "(!)registerOnServer()->sendClientRecipientData(): Failed to send client recipient data\n";
+		return Socket::Status::Error;
 	}
 }
--- MultiplayerProject_Client/NetworkClient.h
+++ MultiplayerProject_Client/NetworkClient.h
@@ -17,7 +17,18 @@ class NetworkClient
 public:
 	NetworkClient();
 
-
+	/*Tries to bind udp data socket to passed port, if failed - endlessly tries to bind any other port
+	Return Status::Done if binded to any port*/
 	Socket::Status init(unsigned short preferablePort);
+
+	Socket::Status registerOnServer(IpAddress serverIp, unsigned short serverRegPort, string clientName);
+
+private:
+	/*Connects registration tcp socket to server registration tcp socket*/
+	Socket::Status connectRegTcpSocket(IpAddress serverIp, unsigned short serverRegPort);
+
+	/*Sets registration tcp socket to be blocking, then sends using it data udp socket port and network client name to 
+	registration socket of a server, so that server create new dedicated client record*/
+	Socket::Status sendClientRecipientData(string clientName);
 };
 
